4dbaffbdfa7c905190775f8941628b256ad8047c
Zethus Studio <105367126+zethus-studio@users.noreply.github.com>
2025-11-19 19:13:27 +0330

    Update Builder import to use MongoDB version

--- src/Traits/HasPermissions.php
+++ src/Traits/HasPermissions.php
@@ -2,7 +2,8 @@
 
 namespace Spatie\Permission\Traits;
 
-use Illuminate\Database\Eloquent\Builder;
+
+use MongoDB\Laravel\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
